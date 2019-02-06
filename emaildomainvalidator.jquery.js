9890209d5e129dfc704517ec51e3af8e72cfa690
Samuel Wagner <mukah@live.com>
2019-02-06 11:31:47 -0200

    Update emaildomainvalidator.jquery.js

--- emaildomainvalidator.jquery.js
+++ emaildomainvalidator.jquery.js
@@ -1,40 +1,38 @@
 /* Made by Samuel Wagner */
 // https://github.com/Mukah/emaildomainvalidator
 
-(function($){
-  $.fn.emailDomainValidator = function(whitelist, success, failed) {
-    whitelist = whitelist || [];
+$.fn.emailDomainValidator = function(whitelist, success, failed) {
+  whitelist = whitelist || [];
 
-    $(this).change(function() {
-      let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-      let email = $(this).val();
-      let domain = email.replace(/.*@/, '');
+  $(this).change(function() {
+    let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    let email = $(this).val();
+    let domain = email.replace(/.*@/, '');
 
-      if(email !== '') {
+    if(email !== '') {
+      $(this)[0].setCustomValidity('');
+
+      if(regex.test(email)) {
         $(this)[0].setCustomValidity('');
 
-        if(regex.test(email)) {
+        if(whitelist.includes(domain)) {
           $(this)[0].setCustomValidity('');
-
-          if(whitelist.includes(domain)) {
-            $(this)[0].setCustomValidity('');
-            success();  
-          } else {
-            $(this)[0].setCustomValidity('O e-mail não pertence a um domínio válido.');  
-            failed();
-          }
+          success();  
         } else {
-          $(this)[0].setCustomValidity('O formato do e-mail não é válido.');
+          $(this)[0].setCustomValidity('O e-mail não pertence a um domínio válido.');  
           failed();
         }
       } else {
-        if(!$(this).prop('required')) {
-          $(this)[0].setCustomValidity('');
-        } else {
-          $(this)[0].setCustomValidity('Preencha este campo.');
-          failed();
-        }
+        $(this)[0].setCustomValidity('O formato do e-mail não é válido.');
+        failed();
       }
-    });
-  };
-}(jQuery));
+    } else {
+      if(!$(this).prop('required')) {
+        $(this)[0].setCustomValidity('');
+      } else {
+        $(this)[0].setCustomValidity('Preencha este campo.');
+        failed();
+      }
+    }
+  });
+};
